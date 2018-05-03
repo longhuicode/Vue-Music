9054595bdbf770f48d84c8fedff7d0e341cf20e1
longhuicode <longhuicode@foxmail.com>
2018-05-03 23:55:16 +0800

    引入 router ,编写好路由配置,引入各个组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill' // 导入填充库,提升兼容性
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 import fastclick from 'fastclick' // 引入 fastclick 解决移动端浏览器 300 毫秒延迟问题
 
 import 'common/stylus/index.styl'
@@ -13,5 +13,6 @@ fastclick.attach(document.body) // 调用 fastclick
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
