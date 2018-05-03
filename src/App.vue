ddb8638266fd89ceea1fefe4cc6daf1d8df0481e
longhuicode <longhuicode@foxmail.com>
2018-05-04 00:16:43 +0800

    完成 Tab.vue 组件, 编写主地址路由跳转

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header/m-header'
+import Tab from 'components/Tab/Tab'
 
 export default {
   name: 'App',
   components: {
-    MHeader
+    MHeader,
+    Tab
   }
 }
 </script>
