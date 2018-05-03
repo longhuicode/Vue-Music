866d1f971df8d203ab5e4465237b9747ec2e9e4e
longhuicode <longhuicode@foxmail.com>
2018-05-03 22:53:01 +0800

    引入 fastclick 解决移动端浏览器 300 毫秒延迟问题

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import 'babel-polyfill' // 导入填充库,提升兼容性
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
+import fastclick from 'fastclick' // 引入 fastclick 解决移动端浏览器 300 毫秒延迟问题
+
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
+
+fastclick.attach(document.body) // 调用 fastclick
 
 /* eslint-disable no-new */
 new Vue({
