7e12ff0c989b2c33cc0cd8a4ea56c00553c4024b
longhuicode <longhuicode@foxmail.com>
2018-05-06 11:52:44 +0800

    封装歌手类singer,获取歌手列表,处理列表数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,80 @@
 <template>
-  <div>
-    歌手页面
+  <div class="singer">
+
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
 export default {
-  name: 'singer'
+  name: 'singer',
+  date() {
+    return {
+      singer: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singer = res.data.list
+          this._normalizeSinger(this.singer)
+        }
+      })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+      })
+      // 为了得到有序列表,我们需要处理 map
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
