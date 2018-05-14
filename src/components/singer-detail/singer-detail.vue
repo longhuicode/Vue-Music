d6e9ba6b1f2947ea459c527e2afa081cbb03b557
longhuicode <longhuicode@foxmail.com>
2018-05-14 21:52:57 +0800

    创建 music-list 组件接受 singer-detail 组件传递的歌手信息

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -9,6 +9,7 @@ import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import {createSong} from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
   name: 'singer-detail',
@@ -18,13 +19,18 @@ export default {
     }
   },
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
   },
   created() {
     this._getDetail()
-    console.log(this.singer)
   },
   methods: {
     _getDetail() {
@@ -37,7 +43,6 @@ export default {
       getSingerDetail(singerId).then((res) => {
         if (res.code === ERR_OK) {
           this.songs = this._normalizeSongs(res.data.list)
-          console.log(this.songs)
         }
       })
     },
@@ -51,21 +56,15 @@ export default {
       })
       return ret
     }
+  },
+  components: {
+    MusicList
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    right: 0
-    left: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active,.slide-leave-active
     transition: all 0.3s
